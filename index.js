3207057810b87afa9297451698d359c86512c43b
mishra06 <mishra.vibhakar7262@gmail.com>
2024-05-29 16:17:41 +0530

    dotenv setup

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const PORT = 10000
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const mishrarouter = require("./router/job");
 
 const server = express();
+dotenv.config();
 
 server.use(express.json());
 
 mongoose
-.connect("mongodb+srv://vibhakar:5Irf7bUTiZnxApS2@jobapp.bcyuxx3.mongodb.net/")  // connection to server
+.connect(process.env.Db_url)  // connection to server
 .then(()=>console.log("connected db ok"))
 .catch((err)=> console.log("Error connect with Db",err));
 
